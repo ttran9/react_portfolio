d31762322ccc36180ff7c1927d67044228b4ec43
ttran9 <toddtran9@gmail.com>
2020-04-01 18:42:49 -0700

    initial upload.

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Portfolio
+
+- This is a React application that I am making to practice deploying to a remote Linux server.
+
+- Demo
+  - TBA (will be up once I upload this to Amazon Lightsail Ubuntu 18.04 LTS)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <span>Boiler Plate Stuff</span>
+      </div>
+    );
+  }
+}
+
+export default App;
