4f2f1b71f493e22efafeb483ac53e4da7f9c6e7a
ttran9 <toddtran9@gmail.com>
2020-04-04 02:42:35 -0700

    constants.

---
+++ src/Constants.js
@@ -0,0 +1,12 @@
+export const HOME_URL = "/";
+export const ABOUT_URL = "/about";
+export const PROJECT_URL = "/projects";
+
+// Github Repositories
+export const CB_FRONT_END_REPO_URL =
+  "https://github.com/ttran9/comp-build-frontend";
+export const CB_BACK_END_REPO_URL =
+  "https://github.com/ttran9/comp-build-backend";
+export const DJ_WF_REPO_URL = "https://github.com/ttran9/pd-weather-forecast";
+export const RAILS_WF_REPO_URL =
+  "https://github.com/ttran9/rails-weather-forecast";
