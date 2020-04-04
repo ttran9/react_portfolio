73155af36918217b88a70cebd26efabbd3cfc8c5
ttran9 <toddtran9@gmail.com>
2020-04-04 02:46:43 -0700

    creating a component to display footer (content at the bottom of the page).

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="container">
+        <hr />
+        <footer className="footer">
+          <p className="text-center">
+            <span>Created by Todd Tran.</span> <br />
+            <span>Still a work in progress....</span>
+          </p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
