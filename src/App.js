514a949a3566667a0d8ca9698b9ce0d6c9152d52
ttran9 <toddtran9@gmail.com>
2020-04-04 02:42:21 -0700

    adding in routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Home from "./components/portfolio/Home";
+import About from "./components/portfolio/About";
+import Project from "./components/portfolio/Project";
+import * as Constants from "./Constants";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <span>Boiler Plate Stuff</span>
-      </div>
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path={`${Constants.HOME_URL}`} component={Home} />
+            <Route exact path={`${Constants.ABOUT_URL}`} component={About} />
+            <Route
+              exact
+              path={`${Constants.PROJECT_URL}`}
+              component={Project}
+            />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
