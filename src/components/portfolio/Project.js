3ad9da019db2362a083e468970e62298f8909a57
ttran9 <toddtran9@gmail.com>
2020-04-07 17:07:11 -0700

    updating project descriptions.

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -7,6 +7,7 @@ class Project extends Component {
       <div className="light-overlay landing-inner text-dark">
         <div className="container">
           <div className="row">
+            <div className="col-sm-3"></div>
             <div className="col-sm-6">
               <div className="card">
                 <div className="card-body">
@@ -14,16 +15,23 @@ class Project extends Component {
                     Computer Build Tracker Frontend
                   </h5>
                   <p className="card-text">
-                    The front-end portion of a full-stack web application built
-                    using ReactJS and Redux. This application displays the web
-                    pages for a user such as a login page, a registration page,
-                    a web page to view a list of computer builds that a user has
-                    created. A logged in user can also view details of the
-                    computer build. JWT authentication is used in order for a
-                    user to make secure API calls to a Spring Boot back-end. One
-                    thing to note is that for simplicity I made it so users can
-                    only view their own builds, however, I plan to change this
-                    feature in the near future.
+                    <p>
+                      A web application allowing a logged in user to create,
+                      modify, and delete computer builds and the associated
+                      details.
+                    </p>
+                    <ul>
+                      <li>
+                        Used React and Redux to present web pages to the logged
+                        in user.
+                      </li>
+                      <li>
+                        Used Bootstrap and CSS to add styling to the web pages.
+                      </li>
+                      <li>
+                        Used JWT Tokens to access secure RESTful endpoints.
+                      </li>
+                    </ul>
                   </p>
                   <a
                     href={`${Constants.CB_FRONT_END_REPO_URL}`}
@@ -34,23 +42,32 @@ class Project extends Component {
                 </div>
               </div>
             </div>
+            <div className="col-sm-3"></div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3"></div>
             <div className="col-sm-6">
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Computer Build Tracker Backend</h5>
                   <p className="card-text">
-                    The back-end portfion of a full-stack web application built
-                    using Java, MySQL, and Spring Boot. Some API endpoints
-                    included consist of allowing a logged in (authenticated)
-                    user be able to create, delete, and update computer builds.
-                    A user can also user update computer build details, such as
-                    the computer parts, and also some notes about the computer
-                    build itself such as build notes (possibly some issues a
-                    user ran into or just some suggestions). These endpoints are
-                    stored inside of a MySQL database which is hosted on Heroku
-                    and accessed by using Hibernate 5. There are also JUnit
-                    tests to verify expected functionality for the operations on
-                    ComputerBuilds and ComputerParts.
+                    <p>
+                      A web application housing an API with endpoints storing
+                      computer builds and its associated details as well as user
+                      credentials.
+                    </p>
+                    <ul>
+                      <li>
+                        Used Spring Security and Hibernate to authenticate users
+                        against a MySQL database and generate a JWT token.
+                      </li>
+                      <li>
+                        Used Spring Boot to create RESTful controllers to
+                        process requests from a React application to create,
+                        update, and delete computer builds and the associated
+                        details.
+                      </li>
+                    </ul>
                   </p>
                   <a
                     href={`${Constants.CB_BACK_END_REPO_URL}`}
@@ -62,34 +79,32 @@ class Project extends Component {
               </div>
             </div>
           </div>
+          <div className="col-sm-3"></div>
           <div className="row">
-            <div class="col-sm-3"></div>
+            <div className="col-sm-3"></div>
             <div class="col-sm-6">
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Django Weather Forecast</h5>
                   <p class="card-text">
-                    A full stack web application built using Python 3, Django 3,
-                    Docker, AWS Lightsail. This application is currently hosted
-                    on an AWS Lightsail Ubuntu virtual machine. I wanted to
-                    build this application so I could get more familiar with
-                    deploying a Django web application to a remote Linux Server
-                    with its own database using Docker. I also generated an SSL
-                    certificate using Certbot in order to support HTTPS so that
-                    the user browsing it would be making secure web requests.
-                  </p>
-                  <p class="card-text">
-                    This web application uses the Google Geocoding API in order
-                    to get a latitude and longitude pair from an entered
-                    address. I also used the Darksky API to return weather
-                    forecasts for the corresponding latitude and longitude. I
-                    used Django's test module to create tests in order to test
-                    that I could make the API calls properly. I also used
-                    Selenium to test at the browser level and I tested scenarios
-                    that the user would go through such as logging in and
-                    logging out. I also tested the user entering in the address
-                    and being able to view the forecasts as well as view
-                    previously forecasts (assuming the user was logged in).
+                    <p>
+                      A web application allowing users to get weather forecasts
+                      for an entered address.
+                    </p>
+                    <ul>
+                      <li>
+                        Created a module to process API calls to RESTful JSON
+                        APIs to return forecasts and location information.
+                      </li>
+                      <li>
+                        Used Django's test module and Selenium to test for
+                        expected functionality.
+                      </li>
+                      <li>
+                        Deployed to a remote Linux server and configured website
+                        to use HTTPS to process secure web requests.
+                      </li>
+                    </ul>
                   </p>
                   <a
                     href={`${Constants.DJ_WF_REPO_URL}`}
@@ -100,7 +115,7 @@ class Project extends Component {
                 </div>
               </div>
             </div>
-            <div class="col-sm-3"></div>
+            <div className="col-sm-3"></div>
           </div>
           <div className="row">
             <div class="col-sm-3"></div>
@@ -109,21 +124,20 @@ class Project extends Component {
                 <div class="card-body">
                   <h5 class="card-title">Rails Weather Forecast</h5>
                   <p class="card-text">
-                    A full-stack web application with a back-end built using
-                    Ruby and Ruby on Rails 5. There were basic tests written
-                    using RSpec. The front-end is build using HTML (Embedded
-                    Ruby Files), CSS (Bootstrap), and JavaScript.
-                  </p>
-                  <p class="card-text">
-                    This web application uses the Google Geocoding API in order
-                    to get a latitude and longitude pair from an entered
-                    address. I used the Darksky API to return weather forecasts
-                    for the corresponding latitude and longitude. I also used
-                    RSpec in order to test for expected functionality such as
-                    being able to view the user's previous searches while logged
-                    in and not logged in. I also tested for cases where the user
-                    would enter something invalid for the address such as a
-                    blank address versus a valid entry.
+                    <p>
+                      A web application allowing users to get weather forecasts
+                      for an entered address.
+                    </p>
+                    <ul>
+                      <li>
+                        Processed results from RESTful JSON APIs to return
+                        forecast and location information.
+                      </li>
+                      <li>
+                        Used RSpec to test searches in a variety of cases.
+                      </li>
+                      <li>Deployed the application to Heroku.</li>
+                    </ul>
                   </p>
                   <a
                     href={`${Constants.RAILS_WF_REPO_URL}`}
